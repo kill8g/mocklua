3c39dc3c8293b930a477801bedf407a86b4406a1
kill8g <yufei@sp.cc>
2023-03-14 15:07:12 +0800

    修改path

--- test.lua
+++ test.lua
@@ -4,7 +4,7 @@ package.cpath = package.cpath .. ";./libs/?.so"
 
 local mock = require "mock"
 array_size = 10
-local test = mock:new("schema/protobuf.parse", "random")
+local test = mock:new("schema.protobuf.parse", "random")
 test:run("./test.proto", "TestProto1", function(data)
     -- socket.send(fd, pack(data))
 end)
